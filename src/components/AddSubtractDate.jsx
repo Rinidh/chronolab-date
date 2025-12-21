266b60809c761efcbc0cf47da94be64655f0e595
Rinidh <rinidhdhokia@gmail.com>
2025-12-21 11:54:53 +0300

    feat: open an accordion to show all inputs about changing date, and strike-though old date and display new one on accepting changes

--- src/components/AddSubtractDate.jsx
+++ src/components/AddSubtractDate.jsx
@@ -5,13 +5,25 @@ import {
   addMonths,
   addWeeks,
   format,
+  isSameDay,
 } from "date-fns";
 import { useWatch } from "react-hook-form";
 
 export const AddSubtractDate = ({ methods }) => {
-  const { register, control, getValues } = methods;
+  const { register, control, getValues, formState } = methods;
   const initialReservationDate = getValues("initialReservationDate");
   const [newDate, setNewDate] = React.useState(initialReservationDate);
+  const [accordionOpen, setAccordionOpen] = React.useState(false);
+
+  const showNewText =
+    formState.touchedFields?.reservationExtension &&
+    !isSameDay(newDate, initialReservationDate) &&
+    !accordionOpen;
+  console.log(
+    formState.touchedFields?.reservationExtension,
+    !isSameDay(newDate, initialReservationDate),
+    !accordionOpen
+  );
 
   const { value = 0, unit = "days" } = useWatch({
     control,
@@ -40,30 +52,50 @@ export const AddSubtractDate = ({ methods }) => {
       <legend>Add & Subtract Dates</legend>
 
       <div>
-        <p>
+        <p style={showNewText ? { textDecoration: "line-through" } : {}}>
           Your reservation is valid upto{" "}
           <span>{format(initialReservationDate, "EEEE, do MMM, yyyy")}</span>
         </p>
-        <span>Change your reservation by:</span>
-        <input
-          type="number"
-          inputMode="numeric"
-          pattern="\d*" // to prevent decimal inputs
-          {...register("reservationExtension.value", {
-            valueAsNumber: true,
-          })}
-        />
-        <select {...register("reservationExtension.unit")}>
-          <option value="days">days</option>
-          <option value="businessDays">business days</option>
-          <option value="weeks">weeks</option>
-          <option value="months">months</option>
-        </select>
+        {showNewText && (
+          <p>
+            Your reservation was changed to{" "}
+            <span>{format(newDate, "EEEE, do MMM, yyyy")}</span>
+          </p>
+        )}
 
-        <p>
-          Your reservation will expire on:{" "}
-          <span>{format(newDate, "EEEE, do MMM, yyyy")}</span>
-        </p>
+        <button type="button" onClick={() => setAccordionOpen(true)}>
+          Change reservation
+        </button>
+        <br />
+
+        {accordionOpen && (
+          <>
+            <span>Change your reservation by:</span>
+            <input
+              type="number"
+              inputMode="numeric"
+              pattern="\d*" // to prevent decimal inputs
+              {...register("reservationExtension.value", {
+                valueAsNumber: true,
+              })}
+            />
+            <select {...register("reservationExtension.unit")}>
+              <option value="days">days</option>
+              <option value="businessDays">business days</option>
+              <option value="weeks">weeks</option>
+              <option value="months">months</option>
+            </select>
+
+            <p>
+              Your reservation will expire on:{" "}
+              <span>{format(newDate, "EEEE, do MMM, yyyy")}</span>
+            </p>
+
+            <button type="button" onClick={() => setAccordionOpen(false)}>
+              Accept
+            </button>
+          </>
+        )}
       </div>
     </fieldset>
   );
